cac6b8bac885cf773becb624c0c4328c21cc269d
MANISHIMWESalton <manishimwejosephs@gmail.com>
2024-09-21 22:33:12 +0200

    [#188307058]Create cards component

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Routes , Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
+import Card from '../components/Card';
 
-const router:React.FC = () => {
+const router: React.FC = () => {
   return (
     <div>
       <Routes>
+        <Route path='/card' element={<Card />}>
+
+        </Route>
         <Route path='/' element={<Layout />}>
         </Route>
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#252836',
+        secondary: '#1F1D2B'
+      }
+    },
   },
   plugins: [],
 }

80eb39b249cdddafd6e7752e422c5b119e6176a3
MANISHIMWESalton <manishimwejosephs@gmail.com>
2024-09-22 12:12:08 +0200

    fixing image and centering text

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,13 @@
 const Card = () => {
     return (
 
-        <div className='flex space-x-2 h-screen bg-primary items-center justify-center'>
-            <div className="w-60 h-82 border border-gray-200 rounded-2xl shadow bg-secondary dark:border-gray-700">
+        <div className='flex space-x-2 h-screen bg-primary items-center'>
+            <div className="w-60 h-82 border border-gray-200 rounded-2xl shadow bg-secondary dark:border-gray-700 ml-10">
                 <div className='flex justify-center items-center'>
-                    <img className='h-40 w-40 rounded-full transform -translate-y-20' src="../../src/assets/plates.jpg" alt="food image" />
+                    <img className='h-40 w-40 rounded-full transform -translate-y-12' src="../../src/assets/plates.jpg" alt="food image" />
                 </div>
 
-                <div className="px-10 pb-5 mt-4 flex flex-col items-center">
+                <div className="px-10 pb-5  flex flex-col items-center">
                     <a href="#">
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Spicy seasoned seafood noodles</h5>
                     </a>
@@ -23,10 +23,10 @@ const Card = () => {
 
             <div className="w-60 h-82 border border-gray-200 rounded-2xl shadow bg-secondary dark:border-gray-700">
                 <div className='flex justify-center items-center'>
-                    <img className='h-40 w-40 rounded-full transform -translate-y-20' src="../../src/assets/plates.jpg" alt="food image" />
+                    <img className='h-40 w-40 rounded-full transform -translate-y-12' src="../../src/assets/plates.jpg" alt="food image" />
                 </div>
 
-                <div className="px-10 pb-5 mt-4 flex flex-col items-center">
+                <div className="px-10 pb-5  flex flex-col items-center">
                     <a href="#">
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Spicy seasoned seafood noodles</h5>
                     </a>
